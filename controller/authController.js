38933ba5585d20e95ea5c595f75e090ff23ef765
ridwan57 <ridwananik57@gmail.com>
2021-04-25 07:20:07 +0600

    4.18

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,5 @@
+import User from "../models/User";
+
 export const home = (req, res, next) => {
   const payload = {
     userLoggedIn: {
@@ -24,23 +26,38 @@ export const register = (req, res, next) => {
   };
   res.render("register", payload);
 };
-export const registerCredentials = (req, res, next) => {
-  console.log("req.body:", req.body);
+
+export const registerCredentials = async (req, res, next) => {
+  // console.log("req.body:", req.body);
   let { firstName, lastName, username, email, password } = req.body;
   firstName = firstName.trim();
   lastName = lastName.trim();
   username = username.trim();
   email = email.trim();
   let payload = req.body;
-  if (firstName && lastName && username && email && password) {
-    payload.errorMessage = "Something went wrong.";
-  } else {
+  if (!firstName || !lastName || !username || !email || !password) {
     payload.errorMessage = " Fillup all value";
     res.render("register", payload);
   }
-  const payload = {
-    userLoggedIn: {
-      firstName: "New title",
-    },
-  };
+
+  const found = await User.findOne({
+    $or: [{ username, email }],
+  }).catch(() => {
+    payload.errorMessage = "User Searching Database Error ";
+    res.render("register", payload);
+  });
+  console.log("found:", found);
+  if (found) {
+    payload.errorMessage = ` ${
+      email === found.email ? "email" : "username"
+    } already in use`;
+    res.render("register", payload);
+  }
+  const newUser = await User.create(req.body).catch(() => {
+    payload.errorMessage = "Creating User Database Error ";
+    res.render("register", payload);
+  });
+
+  payload.errorMessage = `Created User ${newUser}`;
+  res.render("register", payload);
 };
